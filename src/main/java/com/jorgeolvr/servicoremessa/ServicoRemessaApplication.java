f5b985924c45531d191c5bd1a528390d8aa0de34
jorgeolvr <jorge.oliveira@groupsoftware.com.br>
2023-11-08 19:41:34 -0300

    Inicialização do projeto

---
+++ src/main/java/com/jorgeolvr/servicoremessa/ServicoRemessaApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeolvr.servicoremessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoRemessaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoRemessaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jorgeolvr/servicoremessa/ServicoRemessaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeolvr.servicoremessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoRemessaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
